6858bc4fb7cab4c428f04222973469ef451e19e3
rafael-santiago <voidbrainvoid@gmail.com>
2017-09-22 06:06:49 -0800

    License and copyright disclaimer

--- src/test/main.c
+++ src/test/main.c
@@ -1,3 +1,10 @@
+/*
+ *                                Copyright (C) 2017 by Rafael Santiago
+ *
+ * This is a free software. You can redistribute it and/or modify under
+ * the terms of the GNU General Public License version 2.
+ *
+ */
 #include <cutest.h>
 #include "../stack.h"
 #include "../memory.h"
